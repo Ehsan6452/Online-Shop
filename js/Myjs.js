9ed5ed85e9d2b84e1e2cf3092d30d8c18a02baab
Ehsan Hossein Khani <144338242+Ehsan6452@users.noreply.github.com>
2023-11-24 11:42:33 +0330

    Add files via upload

---
+++ js/Myjs.js
@@ -0,0 +1,75 @@
+function Load() {
+    Change_Map('Gorgan');
+    Them('Dark');
+    changeBackground();
+}
+function Them(mode) {
+    switch (mode) 
+    {
+        case 'Light': 
+        {
+            Light();
+            break;
+        }
+        case 'Dark': 
+        {
+            Dark();
+            break;
+        }
+    }
+}
+// 
+
+var backgroundIndex = 0;
+var backgroundImages = ['../img/BackGround_01.jpg', '../img/BackGround_02.jpg', '../img/BackGround_03.jpg'];
+
+function changeBackground() {
+    document.getElementById('Home').style.backgroundImage = 'url(' + backgroundImages[backgroundIndex] + ')';
+    backgroundIndex++;
+    if (backgroundIndex >= backgroundImages.length) {
+        backgroundIndex = 0;
+    }
+}
+
+// Change the background every 5 seconds
+setInterval(changeBackground, 10000);
+
+// 
+function Light() {
+    document.getElementById('Them_Dark').style.display = 'none';
+    document.getElementById('Them_Light').style.display ='flex';
+    // 
+    document.getElementsByTagName('link')[7].href = 'css/Them/Light.css';
+}
+function Dark() {
+    document.getElementById('Them_Dark').style.display = 'flex';
+    document.getElementById('Them_Light').style.display ='none';
+    // 
+    document.getElementsByTagName('link')[7].href = 'css/Them/Dark.css';
+
+}
+function Change_Map(City) {
+    switch (City) {
+        case 'Gorgan':
+            {
+                document.getElementById('Nikolas-Gorgan').style.display = 'flex';
+                document.getElementById('SarzaminCharm-Tehran').style.display = 'none';
+                document.getElementById('Lanka-Mashhad').style.display = 'none';
+                break;
+            }
+        case 'Tehran':
+            {
+                document.getElementById('Nikolas-Gorgan').style.display = 'none';
+                document.getElementById('SarzaminCharm-Tehran').style.display = 'flex';
+                document.getElementById('Lanka-Mashhad').style.display = 'none';
+                break;
+            }
+        case 'Mashhad':
+            {
+                document.getElementById('Nikolas-Gorgan').style.display = 'none';
+                document.getElementById('SarzaminCharm-Tehran').style.display = 'none';
+                document.getElementById('Lanka-Mashhad').style.display = 'flex';
+                break;
+            }
+    }
+}
